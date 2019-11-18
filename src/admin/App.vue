d55eda43349e0df1bf5850992c9d015978e4ebb7
Kirill <kirsglaz@gmai.com>
2019-11-18 21:25:46 +0300

    test 1

---
+++ src/admin/App.vue
@@ -0,0 +1,134 @@
+<template lang="pug">
+  .wrapper        
+    adminheader
+    navigation
+    router-view     
+</template>
+
+<script>
+import { mapState, mapActions, mapGetters } from "vuex";
+import adminheader from "./components/adminheader.vue";
+import navigation from "./components/navigation.vue";
+
+export default {
+  name: "app",
+  components: {
+    adminheader,
+    navigation,        
+  },
+  data() {
+    return {
+      
+    };
+  }
+};
+</script>
+
+<style lang="postcss">
+html {
+  scroll-behavior: smooth;
+}
+body,
+html {
+  height: 100%;
+}
+
+table {
+  padding: 0;
+  margin: 0;
+}
+
+body {
+  font-family: "Open Sans", Helvetica, sans-serif;
+  font-size: 16px;
+  line-height: 1.42;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: $text-color;
+  margin: 0px;
+}
+
+.wrapper {
+  overflow: hidden;
+  min-height: 100%;
+  position: relative;
+}
+
+.container {
+  margin: 0 auto;
+  max-width: 1480px;
+  width: 95%;
+}
+
+.maincontent {
+  height: 100%;
+}
+
+.fullscreen-section {
+  height: 100vh;
+  min-height: 650px;
+}
+
+button {
+  border: 0;
+  cursor: pointer;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+ul {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-weight: normal;
+  margin: 0;
+}
+
+p {
+  margin: 0;
+  padding: 0;
+}
+
+button[type="button"] {
+  cursor: pointer;
+  background-color: transparent;
+}
+
+a {
+  color: inherit;
+  cursor: pointer;
+  text-decoration: none;
+}
+
+img {
+  display: block;
+  max-width: 100%;
+  max-height: 100%;
+}
+
+.title {
+  font-weight: 700;
+  font-size: 36px;
+  color: white;
+}
+
+.error {
+  font-size: 10px;
+  color: red;
+  height: 15px;
+}
+</style>
+
+
+
+
